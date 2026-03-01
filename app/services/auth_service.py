968dcc926bedbf6ca497fc4397eccf9a5ad04d1c
macean20 <maceanchristopher7@gmail.com>
2026-03-01 17:20:56 +0000

    refactor: AuthService avec méthode et attribut d'instance

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -2,18 +2,16 @@ from app.model.user import User
 
 
 class AuthService:
-    _users = [
-        User(1, 'admin',    'admin123'),
-        User(2, 'etudiant', 'etudiant123'),
-    ]
 
-    @staticmethod
-    def login(login: str, password: str):
-        """
-        Vérifie les credentials et retourne l'objet User si valide,
-        sinon retourne None.
-        """
-        for user in AuthService._users:
+    def __init__(self):
+        self.users = [
+            User(1, 'admin',    'admin123'),
+            User(2, 'etudiant', 'etudiant123'),
+            User(3, 'prof',     'prof123'),
+        ]
+
+    def login(self, login: str, password: str):
+        for user in self.users:
             if user.login == login and user.password == password:
                 return user
         return None
