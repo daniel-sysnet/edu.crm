eff6dd7615f755333a74239d3e007a7667fdca64
macean20 <maceanchristopher7@gmail.com>
2026-03-06 00:10:05 +0000

    refactor: paramètres login et password optionnels dans AuthService

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -3,14 +3,13 @@ from app.model.user import User
 
 class AuthService:
 
-    def __init__(self):
-        self.users = [
-            User(1, 'admin',    'admin123'),
-            User(2, 'etudiant', 'etudiant123'),
-            User(3, 'prof',     'prof123'),
+    def __init__(self, users: list[User] = None):
+        self.users = users if users is not None else [
+            User(1, 'admin@gmail.com', 'admin123'),
+            User(2, 'ism@gmail.com', 'ism123'),
         ]
 
-    def login(self, login: str, password: str):
+    def login(self, login: str = None, password: str = None):
         for user in self.users:
             if user.login == login and user.password == password:
                 return user
