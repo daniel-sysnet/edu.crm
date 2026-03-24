f5865aace74d3c0462de90119e9ff81acced7253
macean20 <maceanchristopher7@gmail.com>
2026-03-24 15:25:58 +0000

    fix: get_session_user retourne None si non connecté

--- app/auth/session.py
+++ app/auth/session.py
@@ -13,8 +13,7 @@ def login_user(user) -> None:
 
 def get_session_user() -> dict:
     if SESSION_USER_ID not in session:
-        # return None
-        # Mode test
+        return None
         return {"id": 1, "username": "Jean Dupont", "photo_url": ""}
     return {
         "id":       session[SESSION_USER_ID],
