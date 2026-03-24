f1ef54b6c46a3ffbf10791b5cb968c8e86e3968c
macean20 <maceanchristopher7@gmail.com>
2026-03-24 13:54:26 +0000

    refactor: login par email avec SQLAlchemy

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -4,13 +4,12 @@ from app.models.user import User
 class AuthService:
 
     def __init__(self, users: list[User] = None):
-        self.users = users if users is not None else [
-            User('admin@gmail.com', 'admin123', 'Administrateur'),
-            User('ism@gmail.com', 'ism123', 'Ismail'),
-        ]
+        self.users = users if users is not None else []
 
     def login(self, login: str = None, password: str = None):
-        for user in self.users:
-            if user.login == login and user.password == password:
-                return user
-        return None
+        user = User.query.filter_by(email=login).first()
+        if user is None:
+            return None
+        if not user.check_password(password):
+            return None
+        return user
