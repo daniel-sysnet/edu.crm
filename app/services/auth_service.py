65e2c2342f62bd70301bc74974d061408d4a195f
macean20 <maceanchristopher7@gmail.com>
2026-03-24 16:27:07 +0000

    fix: get_session_user retourne None si non connecté

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -3,13 +3,8 @@ from app.models.user import User
 
 class AuthService:
 
-    def __init__(self, users: list[User] = None):
-        self.users = users if users is not None else []
-
     def login(self, login: str = None, password: str = None):
         user = User.query.filter_by(email=login).first()
-        if user is None:
-            return None
-        if not user.check_password(password):
-            return None
-        return user
+        if user is not None and user.check_password(password):
+            return user
+        return None
